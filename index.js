a8414deec8313ea7a15c4c4fd355922412d0c234
Shridhar <shridharkumbar091@gmail.com>
2025-08-10 09:28:38 +0530

    updated url 2

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const errorHandler = require('./app/middleware/errorHandler');
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "https://kanban-board-frontend-rho.vercel.app",
-  })
-);
+app.use(cors({
+  origin: 'https://kanban-board-frontend-rho.vercel.app', // your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors()); // handle preflight requests
 app.use(express.json());
 
 // Connect to DB
